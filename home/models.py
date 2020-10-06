631d4cc518a6f46a6d8554e3366f9fb3894b46f4
Crowdbotics <team@crowdbotics.com>
2020-10-06 16:22:21 +0000

    Update models

--- home/models.py
+++ home/models.py
@@ -1,3 +1,4 @@
+from django.conf import settings
 from django.db import models
 
 # Create your models here.
@@ -6,27 +7,33 @@ from django.db import models
 
 
 class CustomText(models.Model):
-    title = models.CharField(max_length=150)
+    title = models.CharField(
+        max_length=150,
+    )
 
     def __str__(self):
         return self.title
 
     @property
     def api(self):
-        return f'/api/v1/customtext/{self.id}/'
+        return f"/api/v1/customtext/{self.id}/"
 
     @property
     def field(self):
-        return 'title'
+        return "title"
 
 
 class HomePage(models.Model):
     body = models.TextField()
+    hjhkh = models.BigIntegerField(
+        null=True,
+        blank=True,
+    )
 
     @property
     def api(self):
-        return f'/api/v1/homepage/{self.id}/'
+        return f"/api/v1/homepage/{self.id}/"
 
     @property
     def field(self):
-        return 'body'
+        return "body"
--- web_6_oct_dev_12780/wsgi.py
+++ web_6_oct_dev_12780/wsgi.py
@@ -11,6 +11,6 @@ import os
 
 from django.core.wsgi import get_wsgi_application
 
-os.environ.setdefault('DJANGO_SETTINGS_MODULE', 'web_6_oct_dev_12780.settings')
+os.environ.setdefault("DJANGO_SETTINGS_MODULE", "web_6_oct_dev_12780.settings")
 
 application = get_wsgi_application()
